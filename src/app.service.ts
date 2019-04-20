ac8f6801e6c5251c924adbe00bd02efbcdba1f94
Sam Hunt <samhunt.dev@gmail.com>
2019-04-21 01:17:42 +1200

    fix: Add missing space on landing page powered-by message

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,7 +7,7 @@ export class AppService {
             <h1>Welcome to Parse-the-Parcel</h1>
             <p>${process.env.npm_package_description}</p>
             <p>Check out the <a href="/docs">docs</a> for more information.</p><br/>
-            <p>Version ${process.env.npm_package_version}, powered by<a href="https://nestjs.com/">NestJS</a></p>
+            <p>Version ${process.env.npm_package_version}, powered by <a href="https://nestjs.com/">NestJS</a></p>
         `;
     }
 }
